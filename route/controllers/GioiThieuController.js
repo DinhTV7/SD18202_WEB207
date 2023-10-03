588e3ceeff41f7b5cbfa6002859b76d670925a8a
Ta Van Dinh <dinhtv7@fpt.edu.vn>
2023-10-03 16:05:25 +0700

    update

--- route/controllers/GioiThieuController.js
+++ route/controllers/GioiThieuController.js
@@ -5,4 +5,34 @@ window.GioiThieuController = function ($scope, $routeParams) {
     console.log($routeParams.name);
     let id = $routeParams.id;
     console.log(id);
+
+    // Khai báo mảng
+    $scope.students = [
+        {
+            ten: "Tạ Văn Định",
+            namSinh: 2005,
+            chuyenNganh: "CNTT"
+        },
+        {
+            ten: "DinhTV7",
+            namSinh: 2005,
+            chuyenNganh: "CNTT"
+        }
+    ];
+
+    $scope.createStudent = function () {
+        let flag = false;
+
+        if (flag == true) {
+            // Lấy dữ liệu student nhập vào
+            let newStudent = {
+                ten: $scope.student.name,
+                namSinh: $scope.student.year,
+                chuyenNganh: $scope.student.couse
+            }
+
+            console.log(newStudent);
+            $scope.students.push(newStudent);
+        }
+    }
 }
--- route/controllers/TrangChuController.js
+++ route/controllers/TrangChuController.js
@@ -1,3 +1,22 @@
 window.TrangChuController = function ($scope) {
     $scope.title = "Đây là trang chủ";
+
+    $scope.themMoi = function () {
+        // Lấy ra thông tin từ ô input
+        console.log($scope.inputValue);
+        // Lấy ra từng giá trị
+        console.log($scope.inputValue.name);
+        console.log($scope.inputValue.address);
+    }
+
+    // Bài 1
+    $scope.tinhTong = function () {
+        $scope.sum = parseFloat($scope.number.one) + parseFloat($scope.number.two)
+    }
+
+    // Bài 2
+    $scope.count = 0;
+    $scope.dem = function () {
+        $scope.count++;
+    }
 }
